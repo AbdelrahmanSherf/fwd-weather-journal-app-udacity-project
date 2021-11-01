4ed5e3e8bcbe28ef16c565edabf3a5a36969e332
AbdelrahmanSherf <aabbddoo.sshh@gmail.com>
2021-11-01 18:02:04 +0300

    feat: server running, GET route that returns all projectData, POST route, async func to fetch data from openWeatherAPI

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// // Setup empty JS object to act as endpoint for all routes
+// projectData = {};
 
-// Require Express to run server and routes
+// // Require Express to run server and routes
+
+// // Start up an instance of app
+
+// /* Middleware*/
+// //Here we are configuring express to use body-parser as middle-ware.
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // Cors for cross origin allowance
+
+// // Initialize the main project folder
+// app.use(express.static('website'));
+
+
+// // Setup Server
+
+
+
+
+
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {
+   temperature: 40,
+   data: '',
+   userResponse: '',
+};
+
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const cros = require('cors');
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +45,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cros());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Spin up the server
+const serverPort = 4000;
+app.listen(serverPort, () => {
+   // Callback to debug
+   console.log(`server running on port: ${serverPort}`);
+});
 
-// Setup Server
+/* Initialize all route with a callback function */
+// Respond with JS object when a GET request is made to the alldata route
+app.get('/GetAllData', (req, res) => {
+   res.send(projectData);
+   console.log('made a GET route requestfor all data');
+});
+
+
+/* test */ const addData = [];
+// POST route adds incoming data to projectData 
+   // The POST route should anticipate receiving three pieces of data from the request body
+         // temperature
+         // date
+         // user response
+   // Make sure your POST route is setup to add each of these values with a key to projectData.
+app.post('/addUserData', (req, res) => {
+   console.log(req.body);
+   addData.push(req.body);
+});
+
+// Callback function to complete GET '/all'
+
+// Post Route
+  
--- website/app.js
+++ website/app.js
@@ -1,5 +1,44 @@
 /* Global Variables */
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// const { response } = require("express");
+
+// // Create a new date instance dynamically with JS
+// let d = new Date();
+// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+
+
+// opneWeatherLink
+const opneWeatherLink = 'https://api.openweathermap.org/data/2.5/weather?zip='
+// Personal API Key for OpenWeatherMap API
+const API_KEY = '&appid=2550c62ec56892ef3bf11c0ba456d346';
+// user's intered zip code
+const Sacramento_California = '94203';
+const fullURLTEST = opneWeatherLink + Sacramento_California + API_KEY;
+
+// async function to make a GET request to the OpenWeatherMap API.
+const getWeatherData = async (url) => {
+   const res = await fetch(url);
+
+   try {
+      const data = await res.json();
+      console.log(data);
+   } catch(error) {
+      console.log('error fetching data from open weather', error);
+   }
+}
+getWeatherData(fullURLTEST);
+
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
