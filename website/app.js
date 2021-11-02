9635163c1aa2921f79a85da20072564a4852aa28
AbdelrahmanSherf <aabbddoo.sshh@gmail.com>
2021-11-02 13:42:26 +0300

    feat: retrieve and update UI data dynamically.

--- website/app.js
+++ website/app.js
@@ -56,8 +56,73 @@ const postWeatherData = async (path = '' , postDataObject = {}) => {
    }
 }
 
+
+// Update the UI Dynamically
+const updateUI = async () => {
+   const userFeeling = document.getElementById('feelings').innerText ='lkjfa';
+   const serverResponse = await fetch('/GetAllData');
+   try {
+      const fetchedData = await serverResponse.json();
+      console.log('successfully retrieved all weather data for UI:', fetchedData);
+
+      // update UI with retrieved weather data 
+      document.getElementById('temp').innerText = fetchedData.temperature;
+      document.getElementById('content').innerText = fetchedData.userFeeling;
+      document.getElementById('date').innerText = fetchedData.date;
+   } catch(error) {
+      console.log('could not retrieve all weather data for UI:', error);
+   }
+}
+
+   
 // user's feelings input
-const userFeeling = document.getElementById('feelings').value;
+// const userFeeling = document.getElementById('feelings').value;
+
+
+postWeatherData('/addWeatherData', {
+   temp: 'some degree',
+   date: 'some date',
+   userFeeling: 'some feelings'
+}).then(postWeatherData('/addweatherdata', {
+   temp: 'some degree 2',
+   date: 'some date 2',
+   userFeeling: 'some feelings 2'
+}));
+updateUI('/GetAllData');
+
+
+
+
+
+// // Update the UI Dynamically
+// const updateUI = async (newTemp, newDate, newContent) => {
+//    const temperature = document.getElementById('temp').innerText = newTemp;
+//    const data = document.getElementById('date').innerText = newDate;
+//    const userInput = document.getElementById('content').innerText = newContent;
+// }
+
+// // debugging
+// document.getElementById('generate').addEventListener('click', (event) => {
+//    updateUI('thiss new temp', 'thiss new data', 'thiss new content');
+// });
+
+
+
+
+
+
+
+// TODO: final requsets and DONE!!
+// getWeatherData()
+// .then(postWeatherData())
+// .then(updateUI());
+
+
+
+
+
+
+
 
 // document.body.addEventListener('click', () => {
 //    postWeatherData('/addUserData', postDataObject = {
@@ -68,12 +133,12 @@ const userFeeling = document.getElementById('feelings').value;
 // });
 
 // Event listener for the generate botton, with a callback.
-document.getElementById('generate').addEventListener('click', (event) => {
-   // user's intered zip code
-   const userZipCodeInput = document.getElementById('zip').value;
-   console.log(`user intered: ${userZipCodeInput}`);
-   console.log(getWeatherData(opneWeatherMapLink, userZipCodeInput, API_KEY));
-});
+// document.getElementById('generate').addEventListener('click', (event) => {
+//    // user's intered zip code
+//    const userZipCodeInput = document.getElementById('zip').value;
+//    console.log(`user intered: ${userZipCodeInput}`);
+//    console.log(getWeatherData(opneWeatherMapLink, userZipCodeInput, API_KEY));
+// });
 
 
 
